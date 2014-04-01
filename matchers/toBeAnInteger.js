dc06128b605914d48b607c7d121474ef63a284dd
Matthew Wagerfield <matthew@wagerfield.com>
2014-04-01 20:39:53 +0200

    :gb: Removed IIFE and adding matchers to Jasmine's global scope.

--- matchers/toBeAnInteger.js
+++ matchers/toBeAnInteger.js
@@ -1,17 +1,12 @@
-(function() {
-  var CustomMatchers = {};
-  beforeEach(function() {
-    jasmine.addMatchers(CustomMatchers);
-  });
-  CustomMatchers.toBeAnInteger = function(util, customEqualityTesters) {
-    return {
-      compare: function(actual) {
-        var result = {
-          pass: util.equals(actual, parseInt(actual, 10)),
-          message: 'Expected ' + jasmine.pp(actual) + ' to be an Integer.'
-        };
-        return result;
-      }
-    };
+var CustomMatchers = CustomMatchers || {};
+CustomMatchers.toBeAnInteger = function(util, customEqualityTesters) {
+  return {
+    compare: function(actual) {
+      var result = {
+        pass: util.equals(actual, parseInt(actual, 10)),
+        message: 'Expected ' + jasmine.pp(actual) + ' to be an Integer.'
+      };
+      return result;
+    }
   };
-})();
+};
--- matchers/toOnlyContain.js
+++ matchers/toOnlyContain.js
@@ -1,26 +1,21 @@
-(function() {
-  var CustomMatchers = {};
-  beforeEach(function() {
-    jasmine.addMatchers(CustomMatchers);
-  });
-  CustomMatchers.toOnlyContain = function(util, customEqualityTesters) {
-    return {
-      compare: function(actual, expected) {
-        var result = {
-          pass: util.equals(actual, jasmine.any(Array)),
-          message: 'Expected ' + jasmine.pp(actual) + ' to be an Array.'
-        };
-        if (result.pass) {
-          for (var i = 0; i < actual.length; i++) {
-            if (!util.equals(actual[i], expected)) {
-              result.pass = false;
-              break;
-            }
+var CustomMatchers = CustomMatchers || {};
+CustomMatchers.toOnlyContain = function(util, customEqualityTesters) {
+  return {
+    compare: function(actual, expected) {
+      var result = {
+        pass: util.equals(actual, jasmine.any(Array)),
+        message: 'Expected ' + jasmine.pp(actual) + ' to be an Array.'
+      };
+      if (result.pass) {
+        for (var i = 0; i < actual.length; i++) {
+          if (!util.equals(actual[i], expected)) {
+            result.pass = false;
+            break;
           }
-          result.message = "Expected " + jasmine.pp(actual) + " to only contain " + jasmine.pp(expected) + ".";
         }
-        return result;
+        result.message = "Expected " + jasmine.pp(actual) + " to only contain " + jasmine.pp(expected) + ".";
       }
-    };
+      return result;
+    }
   };
-})();
+};
